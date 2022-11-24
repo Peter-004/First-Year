bd3da7d4f329648d3bcc5b49ff77b516e0629418
Peter-004 <109190204+Peter-004@users.noreply.github.com>
2022-11-23 22:01:05 -0500

    Add files via upload
    Updated js code to greet the user

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
+//Greeting the user
+var greet = document.getElementById("timeofday");
+let day = new Date();
+let hr = day.getHours();
+
+
+if(hr > 1 && hr < 12){
+    greet.innerHTML = "Morning";
+}
+else if(hr > 11 && hr < 19){
+    greet.innerHTML = "Afternoon";
+}
+else{
+    greet.innerHTML = "Evening";
+}
+
 t0ggle = document.querySelector(".t0ggle");
 menucontent = document.querySelector(".menucontents");
 
